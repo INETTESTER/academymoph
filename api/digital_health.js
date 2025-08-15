95465c63c3c3827c68f1f9e97153d2ee4abb79a9
Tester Inet Doc <tester.inet.doc@gmail.com>
2025-08-15 10:35:32 +0700

    z

--- api/digital_health.js
+++ api/digital_health.js
@@ -4,7 +4,9 @@ export function digital_health() {
     const url = 'https://pentest-academymoph.one.th/api/v1/courses/digital-health';
 
     const res = http.get(url);
-
+    if(res.status == 500){
+        console.log(res.body); // Log response body
+    }
     //console.log(res.body); // Log response body
 
     return res; // Return response
--- open.sh
+++ open.sh
@@ -6,10 +6,10 @@
 ##########################################################################
                      API="digital_health" 
                      google_sheet="https://docs.google.com/spreadsheets/d/1IGddKyN1K-r8rNX14ovf23SWGdbAZQyUf99o5MJFnVI/edit?gid=1610289956#gid=1610289956" 
-                     id="1"                 #เปลี่ยน id ทุกครั้งที่ยิง
+                     id="3"                 #เปลี่ยน id ทุกครั้งที่ยิง
                      user="2000";            #จำนวนผู้ใช้งาน
                      duration="1";         #วินาที
-                     scenario="1"           #scenario="1" ยิงเเบบกำหนด request (duration ได้แค่ 1 วินาที)
+                     scenario="3"           #scenario="1" ยิงเเบบกำหนด request (duration ได้แค่ 1 วินาที)
                      cid="1"                #scenario="2" ยิงเเบบกำหนด VUs  (กำหนดว่า user x คน ใช้ระบบ x วินาที)
                                             #scenario="3" ยิงเเบบกำหนด request แต่ไม่แม่นยำ (duration กี่วินาทีก็ได้)
                      status="normal"        #พิมพ์คำว่า "normal" เพื่อยิงโหลดเเละ upload report ไปที่ sheet
